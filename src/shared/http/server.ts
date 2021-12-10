f4d4cf70c82e300637b0979edc669d68d9d4b32a
Francisco dos Passos <chico.81@hotmail.com>
2021-12-10 17:10:39 -0300

    build: adjustment in docker files

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
