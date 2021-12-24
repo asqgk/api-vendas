cb17ee0c5cfbe0f53b59533be0a3342a8106b4ef
Francisco dos Passos <chico.81@hotmail.com>
2021-12-24 10:54:46 -0300

    feat: implement create user and list users services

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,7 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
-import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
   name: string;
--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
-import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 class ListUserService {
   public async execute(): Promise<User[]> {
