cefada7a1ea89eb897d06a111d706df6c9aaedfd
Francisco dos Passos <chico.81@hotmail.com>
2021-12-24 11:05:32 -0300

    style: adjustment in default export class

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import User from '../entities/User';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {
+class UsersRepository extends Repository<User> {
   public async findByName(name: string): Promise<User | undefined> {
     const user = await this.findOne({
       where: {
@@ -33,3 +33,5 @@ export class UsersRepository extends Repository<User> {
     return user;
   }
 }
+
+export default UsersRepository;
