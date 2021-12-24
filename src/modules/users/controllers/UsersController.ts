097a670a83f4cb7ee9b5ae0c097bf16bcf81371a
Francisco dos Passos <chico.81@hotmail.com>
2021-12-24 10:55:15 -0300

    feat: implement users controller

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -6,6 +6,8 @@ export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listUsers = new ListUserService();
 
+    console.log(request.user.id);
+
     const users = await listUsers.execute();
 
     return response.json(users);
