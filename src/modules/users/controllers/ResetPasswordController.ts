2c5d76713be6e8725b7e02cf773ccc34846e5c66
Francisco dos Passos <chico.81@hotmail.com>
2021-12-27 08:07:37 -0300

    feat: implement ResetPasswordController

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { token, password } = request.body;
+
+    const resetPassword = new ResetPasswordService();
+
+    await resetPassword.execute({
+      token,
+      password,
+    });
+
+    // 204 No Content - HTTP
+    return response.status(204).json();
+  }
+}
