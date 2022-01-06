0c003876c9441ec29ba0f6bfa6211660fa437a8d
Francisco dos Passos <chico.81@hotmail.com>
2022-01-06 14:37:42 -0300

    refactor: refactoring class RedisCache

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -1,4 +1,4 @@
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -31,8 +31,6 @@ class UpdateProductService {
     if (productExists && name != product.name) {
       throw new AppError('There is already one product with this name');
     }
-
-    const redisCache = new RedisCache();
 
     await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
